859c148bf49cfbf230e81920ca9d6f7692771b14
Matthew Kobilas <mattkobi789@gmail.com>
2020-07-08 17:26:45 -0400

    Added endpoint allowing user to join channel

--- irc_server/server.go
+++ irc_server/server.go
@@ -13,16 +13,17 @@ import (
 
 // User struct that contains information of users of this irc
 type User struct {
-	Nickname   string  `json:"nickname"`
-	ID         int     `json:"id"`
-	Connection Channel `json:"connection"`
+	Nickname   string `json:"nickname"`
+	ID         int    `json:"id"`
+	Connection string `json:"connection"`
 }
 
 // Channel struct that contains information of various channels
 type Channel struct {
-	ChannelName string `json:"channelname"`
-	ID          int    `json:"id"`
-	Operators   []User `json:"ops"`
+	ChannelName string   `json:"channelname"`
+	ID          int      `json:"id"`
+	Operators   []string `json:"ops"`
+	Connected   []string `json:"connected"`
 }
 
 // Channels map of Channel, where key is channelID (typically Channel.ChannelName
@@ -32,6 +33,20 @@ var Channels = make(map[string]Channel)
 // Users map of User, where key is userID (typically User.Nickname followed by
 // identifier to keep it unique) and value is User
 var Users = make(map[string]User)
+
+func (u User) toString() string {
+	if u.ID == 0 {
+		return u.Nickname
+	}
+	return u.Nickname + strconv.Itoa(u.ID)
+}
+
+func (c Channel) toString() string {
+	if c.ID == 0 {
+		return c.ChannelName
+	}
+	return c.ChannelName + strconv.Itoa(c.ID)
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to our IRC!")
@@ -45,23 +60,32 @@ func createChannel(w http.ResponseWriter, r *http.Request) {
 	}
 	var channel Channel
 	json.Unmarshal(reqBody, &channel)
-	var i int = 0
-	for ok := false; ok; _, ok = Channels[channel.ChannelName+strconv.Itoa(i)] {
-		i++
+	// check createUser for explanation
+	if _, ok := Channels[channel.ChannelName]; !ok {
+		channel.ID = 0
+		Channels[channel.ChannelName] = channel
+	} else {
+		var i int = 0
+		for ok := true; ok; _, ok = Channels[channel.ChannelName+strconv.Itoa(i)] {
+			i++
+		}
+		channel.ID = i
+		Channels[channel.ChannelName+strconv.Itoa(i)] = channel
 	}
-	channel.ID = i
-	Channels[channel.ChannelName+strconv.Itoa(i)] = channel
 	json.NewEncoder(w).Encode(channel)
+	fmt.Println("Endpoint: /channel")
 }
 
 func readAllChannels(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(Channels)
+	fmt.Println("Endpoint: /channels")
 }
 
 func readChannel(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	key := vars["identifier"]
 	json.NewEncoder(w).Encode(Channels[key])
+	fmt.Println("Endpoint: /channel/{identifier}")
 }
 
 func createUser(w http.ResponseWriter, r *http.Request) {
@@ -71,34 +95,82 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 	}
 	var user User
 	json.Unmarshal(reqBody, &user)
-	// check if the user exists in the map
-	// if it does not exist, then add them with the username they requested and
-	// ID 0
-	// otherwise
-	// we check if their username with ID 1 exists, then 2, then 3, etc
-	// until we find one that is not taken, and them to the map with that ID
-	// i.e. if matt is taken, we check for matt1, if that is taken then we
-	// check for matt2, matt2 is not taken, so we add a User with username matt
-	// and ID 2
-	var i int = 0
-	for ok := false; ok; _, ok = Users[user.Nickname+strconv.Itoa(i)] {
-		i++
+	if _, ok := Users[user.Nickname]; !ok {
+		/* check if the user exists in the map
+		if it does not exist, then add them with the username they requested and
+		ID 0 */
+		user.ID = 0
+		Users[user.Nickname] = user
+	} else {
+		/* otherwise
+		we check if their username with ID 1 exists, then 2, then 3, etc
+		until we find one that is not taken, and them to the map with that ID
+		i.e. if matt is taken, we check for matt1, if that is taken then we
+		check for matt2, matt2 is not taken, so we add a User with username matt
+		and ID 2 */
+		var i int = 0
+		for ok := true; ok; _, ok = Users[user.Nickname+strconv.Itoa(i)] {
+			i++
+		}
+		user.ID = i
+		Users[user.Nickname+strconv.Itoa(i)] = user
 	}
-	user.ID = i
-	Users[user.Nickname+strconv.Itoa(i)] = user
 	json.NewEncoder(w).Encode(user)
+	fmt.Println("Endpoint: /user")
 }
 
 func readAllUsers(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(Users)
+	fmt.Println("Endpoint: /users")
 }
 
 func readUser(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	key := vars["identifier"]
 	json.NewEncoder(w).Encode(Users[key])
+	fmt.Println("Endpoint: /user/{identifier}")
 }
 
+func joinChannel(w http.ResponseWriter, r *http.Request) {
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("error joinChannel, reading from request body: %s\n", err)
+	}
+	// get JSON data
+	dat := make(map[string]string)
+	json.Unmarshal(reqBody, &dat)
+	user := Users[dat["user"]]
+	newChannel := Channels[dat["channel"]]
+	// if the user was connected to a channel before this one
+	if user.Connection != "" {
+		// remove user from list of users connected to old channel,
+		// and assign copy back to db
+		oldChannel := Channels[user.Connection]
+		// TODO: may be beneficial to move this into its own function
+		for i, val := range oldChannel.Connected {
+			if val == user.toString() {
+				// https://github.com/golang/go/wiki/SliceTricks#delete-without-preserving-order
+				// deleting user from array without preserving order
+				userCount := len(oldChannel.Connected)
+				oldChannel.Connected[i] = oldChannel.Connected[userCount-1]
+				oldChannel.Connected = oldChannel.Connected[:userCount-1]
+				break
+			}
+		}
+		Channels[user.Connection] = oldChannel
+	}
+	// change user's channel connection, and assign the copy back to db
+	user.Connection = newChannel.toString()
+	Users[dat["user"]] = user
+	// add user to list of users connected to new channel,
+	// and assign copy back to db
+	newChannel.Connected = append(newChannel.Connected, user.toString())
+	Channels[dat["channel"]] = newChannel
+	json.NewEncoder(w).Encode(Channels[dat["channel"]])
+	fmt.Println("Endpoint: /join")
+}
+
+// handles different requests using Gorilla mux router
 func handleRequests() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homePage)
@@ -108,6 +180,7 @@ func handleRequests() {
 	router.HandleFunc("/user", createUser).Methods("POST")
 	router.HandleFunc("/users", readAllUsers)
 	router.HandleFunc("/user/{identifier}", readUser)
+	router.HandleFunc("/join", joinChannel).Methods("POST")
 	log.Fatalln(http.ListenAndServe(":7777", router))
 }
 
@@ -116,44 +189,37 @@ func main() {
 		"General": Channel{
 			ChannelName: "General",
 			ID:          0,
-			Operators: []User{
-				User{
-					Nickname: "Kobo",
-					ID:       0,
-				},
-				User{
-					Nickname: "DardarBinks",
-					ID:       0,
-				},
-				User{
-					Nickname: "Jass",
-					ID:       0,
-				},
+			Operators: []string{
+				"Kobo",
+				"DarDarBinks",
+				"Jass",
 			},
+			Connected: []string{},
 		},
 		"SumDumShiet": Channel{
 			ChannelName: "SumDumShiet",
 			ID:          0,
-			Operators: []User{
-				User{
-					Nickname: "Bobo",
-					ID:       0,
-				},
+			Operators: []string{
+				"Bobo",
 			},
+			Connected: []string{},
 		},
 	}
 	Users = map[string]User{
 		"Matt": User{
-			Nickname: "Matt",
-			ID:       0,
+			Nickname:   "Matt",
+			ID:         0,
+			Connection: "",
 		},
 		"Darius": User{
-			Nickname: "Darius",
-			ID:       0,
+			Nickname:   "Darius",
+			ID:         0,
+			Connection: "",
 		},
 		"Jasmine": User{
-			Nickname: "Jasmine",
-			ID:       0,
+			Nickname:   "Jasmine",
+			ID:         0,
+			Connection: "",
 		},
 	}
 	handleRequests()

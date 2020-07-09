b7f7e680127327917cff458baa7dd99ecd3d8ff2
DHFiallo <noreplee@gmail.com>
2020-07-08 22:32:09 -0400

    Fixed operator

--- irc_client/client.go
+++ irc_client/client.go
@@ -1,28 +1,28 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"encoding/json"
 	"fmt"
-	"bytes"
-	//"log"
-	"net/http"
 	"io/ioutil"
-	"bufio"
+	"net/http"
 	"os"
 	"strings"
 )
+
 type User struct {
 	Nickname string `json:"nickname"`
 	ID       int    `json:"id"`
 }
+
 // Channel struct that contains information of various channels
 type Channel struct {
-	ChannelName string `json:"channelname"`
-	ID          int    `json:"id"`
-	Operators   []string `json:"ops"`
+	ChannelName string   `json:"channelname"`
+	ID          int      `json:"id"`
+	Operators   []string `json:"operators"`
 	Connected   []string `json:"connected"`
 }
-
 
 //Done
 func showAllChannels() string {
@@ -33,27 +33,27 @@ func showAllChannels() string {
 		return "The HTTP request failed with error"
 	} else {
 		data, _ := ioutil.ReadAll(response.Body)
-    	return string(data)
+		return string(data)
 	}
 }
 
 //Done
 func createChannel(channelName string, names ...string) {
-	
-	fmt.Println(names)
 
-	jsonData := Channel{ChannelName:channelName,
-		ID:0,
-		Operators:names,
-		Connected:[]string{},
+	jsonData := Channel{ChannelName: channelName,
+		ID:        0,
+		Operators: names,
+		Connected: []string{},
 	}
+
+	fmt.Println(jsonData)
 	jsonValue, _ := json.Marshal(jsonData)
 	response, err := http.Post("http://100.1.219.194:7777/channel", "application/json", bytes.NewBuffer(jsonValue))
-    if err != nil {
-        fmt.Printf("The HTTP request failed with error %s\n", err)
-    } else {
-        data, _ := ioutil.ReadAll(response.Body)
-        fmt.Println(string(data))
+	if err != nil {
+		fmt.Printf("The HTTP request failed with error %s\n", err)
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		fmt.Println(string(data))
 	}
 }
 
@@ -62,27 +62,25 @@ func joinChannel(channelName string, name string) { //Done
 	jsonData := map[string]string{"user": name, "channel": channelName}
 	jsonValue, _ := json.Marshal(jsonData)
 	response, err := http.Post("http://100.1.219.194:7777/join", "application/json", bytes.NewBuffer(jsonValue))
-    if err != nil {
-        fmt.Printf("The HTTP request failed with error %s\n", err)
-    } else {
+	if err != nil {
+		fmt.Printf("The HTTP request failed with error %s\n", err)
+	} else {
 		data, _ := ioutil.ReadAll(response.Body)
 		fmt.Println(string(data))
-        fmt.Println("Welcome to " + channelName + ", " + name)
+		fmt.Println("Welcome to " + channelName + ", " + name)
 	}
 }
 
-func main(){
-
+func main() {
 
 	response, err := http.Get("http://100.1.219.194:7777/")
 	if err != nil {
-    	fmt.Printf("The HTTP request failed with error %s\n", err)
+		fmt.Printf("The HTTP request failed with error %s\n", err)
 	} else {
-    	data, _ := ioutil.ReadAll(response.Body)
-    	fmt.Println(string(data))
+		data, _ := ioutil.ReadAll(response.Body)
+		fmt.Println(string(data))
 	}
 
-	
 	var resp []string
 	var loop bool = false
 
@@ -91,19 +89,25 @@ func main(){
 		if scanner.Scan() {
 			line := scanner.Text()
 			resp = strings.Split(line, " ")
+			fmt.Println(resp)
 		}
 		switch resp[0] {
+		case "/help":
+			fmt.Println("/create [ChannelName] [Name1] [Name2] [Name3...] creates a channel, if one already exists then creates a 2nd one for it. Subsequent names are operators for the channel. Must have at least 1")
+			fmt.Println("/channels    shows all channels")
+			fmt.Println("/join [ChannelName] [UserName]  joins that respect channel under that username")
+			fmt.Println("/exit  exits the program")
 		case "/channels": //Done
 			result := showAllChannels()
 			fmt.Println(result)
 		case "/create": //Done
-			if(resp[1] == "channel" && len(resp) >= 3){
+			if resp[1] == "channel" && len(resp) >= 3 {
 				createChannel(resp[2], resp[3:]...)
 			} else {
 				fmt.Println("Error. Failed /create call. Check out /help for more info")
 			}
 		case "/join": //Done
-			if(len(resp) == 3) {
+			if len(resp) == 3 {
 				joinChannel(resp[1], resp[2])
 			} else {
 				fmt.Println("Error. Failed /join call. Check out /help for more info")
@@ -112,5 +116,5 @@ func main(){
 			loop = true
 		}
 	}
-	
+
 }
